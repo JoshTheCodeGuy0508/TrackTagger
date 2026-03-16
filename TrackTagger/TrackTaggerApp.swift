de3f3f9167c7e86a3d225c5c433575ee04597aa7
JoshTheCodeGuy0508 <m9cpcnkhdd@privaterelay.appleid.com>
2026-03-16 15:59:19 -0400

    Initial Commit

---
+++ TrackTagger/TrackTaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackTaggerApp.swift
+//  TrackTagger
+//
+//  Created by Joshua Varnum on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct TrackTaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackTaggerTests/TrackTaggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackTaggerTests.swift
+//  TrackTaggerTests
+//
+//  Created by Joshua Varnum on 3/16/26.
+//
+
+import Testing
+@testable import TrackTagger
+
+struct TrackTaggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
